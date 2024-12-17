1ac2410136f53b74fd9a3f1716707d3efb893079
José Manuel Miret Martín <josmirmar2@alum.us.es>
2024-12-17 18:30:18 +0100

    cambio 1

--- EjercicioRS/manage.py
+++ EjercicioRS/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_recommendation.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EjercicioRS.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
